be5d0a4a3bfd771b2fe8b0f2e005a71a63146d7b
kesihinz <keishin.chou@gmail.com>
2019-11-01 19:13:38 +0900

    Initial Commit

---
+++ WhiteHousePetitions-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhiteHousePetitions-2
+//
+//  Created by Keishin CHOU on 2019/11/01.
+//  Copyright © 2019 Keishin CHOU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
